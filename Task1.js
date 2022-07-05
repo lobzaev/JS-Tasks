0fb0d5f750fd695322aab3b935f52b852cf34709
Yuri Lobzaev <44752333+lobzaev@users.noreply.github.com>
2022-07-05 14:34:26 +0200

    Create Task1.js

---
+++ Task1.js
@@ -0,0 +1,44 @@
+/**
+ * Outputs the number of characters in the passed string in console
+ * @param string - string 
+ * example output in console:
+ * "Your string has 23 characters."
+ */
+function howMany(string){
+		/* ??? */	
+};
+
+howMany("Hello Developer!");
+
+
+
+
+
+/**
+ * Outputs a greeting string in console
+ * @param name 
+ * @param age 
+ * @param city 
+	* example output in console:
+ * "Hi, my name is Max. I am from Berlin and i am 25 years old."
+ */
+function sayHello(name,age,city){
+	/* ??? */	
+};
+
+sayHello('Max',25,'Berlin');
+
+
+
+/**
+ * returns the square of a passed number
+ * @param num - number
+ * 
+ * example output: 9
+ */
+
+function getSquere(num){
+	/* ??? */	
+};
+
+console.log( getSquere(3) );
